81e0b2927bcb540f4a55a1d91e7b23efd72cd01e
Andron-debug <b.a.a.2002@yandex.ru>
2021-04-03 14:20:39 +0300

    Добалена клавиатура

--- Coursework/Main.Designer.cs
+++ Coursework/Main.Designer.cs
@@ -35,8 +35,9 @@ namespace Coursework
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(650, 450);
             this.Name = "Main";
+            this.Load += new System.EventHandler(this.Main_Load);
             this.ResumeLayout(false);
 
         }
--- Coursework/Main.cs
+++ Coursework/Main.cs
@@ -17,5 +17,22 @@ namespace Coursework
             InitializeComponent();
         }
 
+        Button[] keyboard = new Button[32];
+        private void Main_Load(object sender, EventArgs e)
+        {
+            this.Width = 668;
+            for (int i = 0; i < 32; i++)
+            {
+                keyboard[i] = new Button();
+                keyboard[i].Width = 40;
+                keyboard[i].Height = 40;
+                if (i< 16) keyboard[i].Top = 350;
+                else keyboard[i].Top = keyboard[0].Top+keyboard[0].Height;
+                keyboard[i].Left = keyboard[i].Width*(i%16)+5;
+                //"а" имеет номер 1072, "А" - 1040
+                keyboard[i].Text = Convert.ToString(Convert.ToChar(1040 + i));
+                Controls.Add(keyboard[i]);
+            }
+        }
     }
 }
